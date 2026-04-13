2678936aef3d947b75e31b398921173691a74140
evilevik92-cpu <evilevik92@gmail.com>
2026-04-13 16:51:56 +0530

    Add files via upload

---
+++ com.java
@@ -0,0 +1,21 @@
+package studentreg;
+
+public class com { 
+	 
+	public class Main {
+	  public static void main(String[] args) {
+	      System.out.println("Java program executed using Jenkins CI");
+	      System.out.println("Student name : jake");
+	  	  System.out.println("Reg.no = 23MIC0007");
+	  	  System.out.println("Course Name = application development");
+	  	  System.out.println("Department = CSE");
+	  	  System.out.println("Experiment Title = jenkins");
+	  	  System.out.println("Date = 12-04-2026");
+	  	  System.out.println("Message = Library Build Successful");
+	  	 System.out.println("Faculty name = prof.sathya");
+	  	 System.out.println("Lab Slot = L34+L35");
+	  	  
+	  }
+	}
+
+}
